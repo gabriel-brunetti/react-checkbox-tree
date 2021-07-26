a72a3c8d56d35be403a6eb4e4c8013258374a282
Gabriel Brunetti <gabriel.brunetti1@gmail.com>
2021-07-26 09:08:05 -0300

    check no localStorage

--- src/components/Item/index.js
+++ src/components/Item/index.js
@@ -1,22 +1,64 @@
 import React, { useState, useEffect } from 'react'
+import List from '../List'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faChevronDown } from '@fortawesome/free-solid-svg-icons'
 import './style.css'
 
-const Item = ({ id, name, children, level }) => {
+const Item = ({ id, name, children }) => {
   const [collapsed, setCollapsed] = useState(true)
   const [checked, setChecked] = useState(false)
+  const checkedIds = JSON.parse(localStorage.getItem('check')) || []
 
   children = Object.entries(children).map((item) => {
     return item[1]
   })
 
+  useEffect(() => {
+    const expandedIds = JSON.parse(localStorage.getItem('expanded')) || []
+    expandedIds.forEach((item) => {
+      if (item === id) {
+        setCollapsed(false)
+      }
+    })
+  }, [id])
+
+  useEffect(() => {
+    const expandedIds = JSON.parse(localStorage.getItem('expanded')) || []
+    if (!collapsed) {
+      localStorage.setItem('expanded', JSON.stringify([...expandedIds, id]))
+    } else {
+      const items = expandedIds.filter((idEx) => idEx !== id)
+      localStorage.setItem('expanded', JSON.stringify(items))
+    }
+  }, [collapsed, id])
+
+  useEffect(() => {
+    checkedIds.forEach((item) => {
+      if (item === id) {
+        setChecked(true)
+      }
+    })
+  }, [checkedIds, id])
+
   const toggleCollapse = (e) => {
-    setCollapsed(!collapsed)
+    setCollapsed((val) => !val)
   }
 
   const toggleChecked = () => {
-    setChecked(!checked)
+    let checkedStatus = !checked
+    const childrenId = children.map((children) => children.id)
+    const checkedItems = JSON.parse(localStorage.getItem('check')) || []
+    if (checkedStatus) {
+      localStorage.setItem(
+        'check',
+        JSON.stringify([...checkedItems, id, ...childrenId])
+      )
+    } else {
+      let items = checkedItems.filter((idItem) => idItem !== id)
+      console.log(checkedItems)
+      localStorage.setItem('check', JSON.stringify(items))
+    }
+    setChecked(checkedStatus)
   }
 
   return (
@@ -37,12 +79,8 @@ const Item = ({ id, name, children, level }) => {
       )}
 
       <ul className={collapsed ? 'collapsed' : ''}>
-        {children.map((person, id) => {
-          return (
-            <li key={id}>
-              <Item key={person.id} {...person} />
-            </li>
-          )
+        {children.map((item, id) => {
+          return <li key={id}>oi'</li>
         })}
       </ul>
     </div>
