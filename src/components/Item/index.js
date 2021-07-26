b5b0638a4d6ebe52eae0812fd9177e55493ef939
Gabriel Brunetti <gabriel.brunetti1@gmail.com>
2021-07-26 04:40:13 -0300

    added checkboxes, css draft, collapsable elements

--- src/components/Item/index.js
+++ src/components/Item/index.js
@@ -1,16 +1,42 @@
 import React, { useState, useEffect } from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faChevronRight } from '@fortawesome/free-solid-svg-icons'
+import { faChevronDown } from '@fortawesome/free-solid-svg-icons'
+import './style.css'
 
 const Item = ({ id, name, children, level }) => {
+  const [collapsed, setCollapsed] = useState(true)
+  const [checked, setChecked] = useState(false)
+
   children = Object.entries(children).map((item) => {
     return item[1]
   })
 
+  const toggleCollapse = (e) => {
+    setCollapsed(!collapsed)
+  }
+
+  const toggleChecked = () => {
+    setChecked(!checked)
+  }
+
   return (
-    <article>
-      <p>{name}</p>
-      <ul>
+    <div className='item-container'>
+      <div className='checkbox-container' onClick={toggleChecked}>
+        <input
+          type='checkbox'
+          name='checkbox'
+          id='checkbox'
+          checked={checked}
+          onChange={() => setChecked(!checked)}
+        />
+        <span>{name}</span>
+      </div>
+
+      {children.length > 0 && (
+        <FontAwesomeIcon icon={faChevronDown} onClick={toggleCollapse} />
+      )}
+
+      <ul className={collapsed ? 'collapsed' : ''}>
         {children.map((person, id) => {
           return (
             <li key={id}>
@@ -19,7 +45,7 @@ const Item = ({ id, name, children, level }) => {
           )
         })}
       </ul>
-    </article>
+    </div>
   )
 }
 
